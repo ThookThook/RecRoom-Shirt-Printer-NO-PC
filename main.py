74d37b7fbac9e78dbc9242223b2e135fd57c1a51
ThookThook <159074935+ThookThook@users.noreply.github.com>
2024-04-26 15:03:54 -0700

    Updated main.py
    formatted the code better and separated some code into other files

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from pathlib import Path
 from typing import Tuple, List
 from PIL import Image, ImageFilter  # PIL is the only required import
 
+import colorCompiler;
 
 # Varibles:
 NAME = "Color Compiler + Mcren's String Importer"
 VERSION = "1.00"
 IMAGE_PATH = "Image.png"
+img = Image.open(IMAGE_PATH); # The image
 
-# Color Compiler's symbols
-SYMBOLS = ['!','','','#','','','$','','','%','','','&','','','(','','',')','','','*','','','+','','',',','','','.','','','/','','',':','','',';','','','<','','','=','','','>','','','?','','','@','','','[','','','Ñ','','',']','','','^','','','_','','','{','','','|','','','}','','','~','','','¢','','','£','','','¤','','','¥','','','¦','','','§','','','¨','','','©','','','ª','','','«','','','¬','','','Ö','','','®','','','¯','','','°','','','±','','','²','','','³','','','´','','','µ','','','¶','','','·','','','¸','','','¹','','','º','','','»','','','¼','','','½','','','¾','','','¿','','','À','','','È','','','ß','','','Ä','','','ê','','','ö','','','Ø','','','Ð','','','Ý','','','ä','','','î','','','Œ','','','Ç','','','Ž','','','ÿ','','','Ú','','','É','','','Ê','','','Æ','','','Ë','','','Ù','','','Ü','','','a','','','ƒ','','','ñ','','','å','','','Å','','','ë','','','Ï','','','ï','','','ù','','','ý','','','Ã','','','Â','','','ž','','','Á','','','Ò','','','Ì','','','Í','','','Ó','','','Ô','','','Õ','','','€','','','Š','','','†','','','‡','','','™','','','š','','','œ','','','Ÿ','','','Û','','','ã','','','â','','','ð','','','õ','','']
+# Color Compiler's symbols 
+SYMBOLS = ('!','','','#','','','$','','','%','','','&','','','(','','',')','','','*','','','+','','',',','','','.','','','/','','',':','','',';','','','<','','','=','','','>','','','?','','','@','','','[','','','Ñ','','',']','','','^','','','_','','','{','','','|','','','}','','','~','','','¢','','','£','','','¤','','','¥','','','¦','','','§','','','¨','','','©','','','ª','','','«','','','¬','','','Ö','','','®','','','¯','','','°','','','±','','','²','','','³','','','´','','','µ','','','¶','','','·','','','¸','','','¹','','','º','','','»','','','¼','','','½','','','¾','','','¿','','','À','','','È','','','ß','','','Ä','','','ê','','','ö','','','Ø','','','Ð','','','Ý','','','ä','','','î','','','Œ','','','Ç','','','Ž','','','ÿ','','','Ú','','','É','','','Ê','','','Æ','','','Ë','','','Ù','','','Ü','','','a','','','ƒ','','','ñ','','','å','','','Å','','','ë','','','Ï','','','ï','','','ù','','','ý','','','Ã','','','Â','','','ž','','','Á','','','Ò','','','Ì','','','Í','','','Ó','','','Ô','','','Õ','','','€','','','Š','','','†','','','‡','','','™','','','š','','','œ','','','Ÿ','','','Û','','','ã','','','â','','','ð','','','õ','','')
 
 # Actual symbols list without spaces.
-SYMBOLS_LIST = ["!","#","$","%","&","(",")","*","+",",",".","/",";",":","<","=",">","?","@","[","Ñ","]","^","_","{","|","}","~","¢","£","¤","¥","¦","§","¨","©","ª","«","¬","Ö","®","¯","°","±","²","³","´","µ","¶","·","¸","¹","º","»","¼","½","¾","¿","À","È","ß","Ä","ê","ö","Ø","Ð","Ý","ä","î","Œ","Ç","Ž","ÿ","Ú","É","Ê","Æ","Ë","Ù","Ü","a","ƒ","ñ","å","Å","ë","Ï","ï","ù","ý","Ã","Â","ž","Á","Ò","Ì","Í","Ó","Ô","Õ","€","Š","†","‡","™","š","œ","Ÿ","Û","ã","â","ð","õ"]
+SYMBOLS_LIST = ("!","#","$","%","&","(",")","*","+",",",".","/",";",":","<","=",">","?","@","[","Ñ","]","^","_","{","|","}","~","¢","£","¤","¥","¦","§","¨","©","ª","«","¬","Ö","®","¯","°","±","²","³","´","µ","¶","·","¸","¹","º","»","¼","½","¾","¿","À","È","ß","Ä","ê","ö","Ø","Ð","Ý","ä","î","Œ","Ç","Ž","ÿ","Ú","É","Ê","Æ","Ë","Ù","Ü","a","ƒ","ñ","å","Å","ë","Ï","ï","ù","ý","Ã","Â","ž","Á","Ò","Ì","Í","Ó","Ô","Õ","€","Š","†","‡","™","š","œ","Ÿ","Û","ã","â","ð","õ")
 
 # Mcren's String encoder colors/symbols: 
 # All of RecRoom color in order + tan marker + eraser
@@ -85,156 +87,22 @@ MaxStringLength: int = 512  # Maximum length string
 
 
 # Typing alias for color
-PixelColor = Tuple[int, int, int]
-
-
-
-# the color comipler
-def colorCompiler(symbols):
-  print("")
-  print(str(NAME) + " " + str(VERSION))
-  selection = str(input("Do you want to compile? (y/n):\n "))
-  if (selection in ["y", "Y", "yes", "Yes", "YES", "yes.", "Yes.", "YES."]):
-      print("  Select image file to compile")
-  else:
-      return
-
-  imgPath = IMAGE_PATH
-  try: #File error detection
-      try:
-          img = Image.open(imgPath)
-      except:
-          print("")
-          print("Invalid image type - only use PNG, JPEG, or JFIF")
-          print("")
-          quit()
-  except AttributeError:
-      print("Error - file window closed")
-      print("")
-      quit()
-
-  img = img.convert('RGB')
-  imgPath = str(imgPath)
-  imgPath = imgPath.split('/')
-  imgPath = imgPath[len(imgPath)-1]
-  try:
-      imgPath = imgPath.replace('.png','')
-  except:
-      try:
-          imgPath = imgPath.replace('.jpg','')
-      except:
-          try:
-              imgPath = imgPath.replace('.jfif','')
-          except:
-              try:
-                  imgPath = imgPath.replace('.jpeg','')
-              except:
-                  pass
-
-  print("  1 marker is minmum 113 is maximum")
-  img.load()
-  coloramount = int(input("Enter amount of colors: "))
-  if (coloramount < 1):
-      print("")
-      print("Color amount too small - 1 is minmum amount")
-      print("")
-      quit()
-  elif (coloramount > 113):
-      print("")
-      print("Color amount too large - 113 is maximum amount")
-      print("")
-      quit()
-  else:
-      pass
-  print("   0 = Median Cut - Has best color detection")
-  print("   1 = Maximum Coverage - Use for special circumstances")
-  print("   2 = Fast Octree - Use this method it works the best")
-  colortype = int(input("Enter type of quantization: "))
-  try:
-      quant = img.quantize(colors=coloramount,method=colortype)
-  except ValueError:
-      print("")
-      print("Quantization method not recognized - enter valid integer")
-      print("")
-      quit()
-
-  count = coloramount*3
-  palette = (quant.getpalette()[:count])
-  maximum = int(len(palette)-2)
-  hexcolors = []
-  rgbcolors = []
-  i = 0
-  if (coloramount > 0 and coloramount <= 113):
-      for i in (range(maximum)):
-          if (i%3 == 0):
-              rgb = palette[i],palette[i+1],palette[i+2]
-              hexcolor = str(rgbToHex(rgb))
-              rgbcolors.append(str(rgb) + ': "' + str(symbols[i]) + '",')
-              hexcolors.append(hexcolor)
-              i += 1
-          else:
-              i += 1
-  else:
-      for i in (range(maximum)):
-          if (i%3 == 0):
-              rgb = palette[i],palette[i+1],palette[i+2]
-              hexcolor = str(rgbToHex(rgb))
-              rgbcolors.append(str(rgb))
-              hexcolors.append(hexcolor)
-              i += 1
-          else:
-              i += 1
-  print(str(coloramount) + " colors compiled")
-  print("")
-  heximgPath = "Hex Colors.txt"
-  with open(heximgPath, 'w') as file1:
-      file1.write('\n'.join(hexcolors))
-  print("Text file written & colors set")
-  dict = {}
-  for elem in rgbcolors:
-    dict[eval(elem[0:elem.index(":")])] = elem[elem.index(":")+3]
-  return dict
-
-
-# color compiler functions:
-def rgbToHex(rgb):
-  return('#%02x%02x%02x' % rgb)
-
-def hexToRGB(hex):
-  hex = str(hex)
-  hex = hex.replace('#','')
-  return(tuple(int(hex[i:i+2], 16) for i in (0, 2, 4)))
-
-def printBreak(number):
-  for i in range(number):
-      print("")
-
-
-def dilate(cycles, image):
-  for i in range(cycles):
-      image = image.filter(ImageFilter.MaxFilter(3))
-      return image
-
-def erode(cycles, image):
-  for i in range(cycles):
-      image = image.filter(ImageFilter.MinFilter(3))
-      return image
-
-
-
-
+PixelColor = Tuple[int, int, int];
 
 
 
 # Function to write compiled strings to a text file
         # Function to write compiled strings to a text file
 def write_strings_to_file(strings, max_length=512):
-  with open("Image Data.txt", "w", encoding="UTF-8") as file:
+  with open("Image Data 1.txt", "w", encoding="UTF-8") as file:
       for i in range(0, len(strings), max_length):
           file.write(("\n".join(strings[i:i + max_length]) + "\n").strip())
   with open("Image Data 2.txt", "w", encoding="UTF-8") as file:
     for i in range(0, len(strings), max_length):
         file.write(("\n\n".join(strings[i:i + max_length]) + "\n\n").strip())
+  with open("Image Data.txt", "w", encoding="UTF-8") as file:
+    for ln in strings:
+      file.write(ln);
 
 
 
@@ -286,10 +154,6 @@ def quantize(img, ask_for_dither: bool = True, dither: int = 0, open_image: bool
     palette_image.putpalette(ALL_COLORS)
     new_image = img.quantize(palette=palette_image, dither=dither).convert("RGB")
 
-    if open_image:
-        print("Opening the final image...")
-        new_image.show()
-
     new_image.save("Preview Image.png", format="PNG")
     return new_image
 
@@ -331,9 +195,6 @@ def encode(img: Image, vertical_print: bool = False, dither_: bool = True):
             pixel_color.append(p)
         # Print the progress
         progress_update(y + 1, img, "Encoding")
-
-    if dither and dither_:
-        full_image.show()
 
     colors: List[Tuple[int, str]] = []
     count: int = 0
@@ -386,20 +247,21 @@ def stringEncoder(list_size: int):
     # Print amount of {`MaxStringLength`} char long strings, image dimensions and total `List Create`s needed.
     print(f"\nGenerated {len(img_data)} strings for image WxH {img.width}x{img.height}")
     print(f"Space needed: {len(img_data) // list_size} Lists (+ {len(img_data) % list_size})")
-  
+
     return img_data
 # end
 
 #colorCompiler(SYMBOLS)
 
 option = int(input("""Select color option:
-1.Aquire them from image.
-2.Input colors manually.
-3.Use default colors from the Rec Room color pallete.\n\nanswer: """));
+1. Aquire them from image.
+2. Input colors manually.
+3. Import from text file.
+4. Use default colors from the Rec Room color pallete.\n\nanswer: """));
 
 # Using the color compiler
 if option == 1:
-  rrPalette = colorCompiler(SYMBOLS);
+  rrPalette = colorCompiler.main(SYMBOLS,  IMAGE_PATH);
 
 #inputing colors manually
 elif option == 2:
@@ -408,20 +270,24 @@ elif option == 2:
   print("Please enter colors in the format of \"(R,G,B)\",\npress enter to stop\n there currently is a max of 113 colors availible.");
   rrPalette = {};
   for i in SYMBOLS_LIST:
-    #try:
-    print(f"{count} colors available...")
-    color = input("Enter a color: ");
-    if(color == ""): break
-    rgbString += str(rgbToHex(eval(color))) + "\n";
-    rrPalette[eval(color)] = i;
-    --count; # count-- doesn't work for some reason???
-    with open("Hex Colors.txt", "w", encoding="UTF-8") as file:
-        file.write(rgbString);
-    except:
-        print("Error!! Please try restart the program and input the color values correctly, thank you.");
-        time.sleep(4);
-        exit();
+    try:
+      print(f"{count} colors available...")
+      color = input("Enter a color: ");
+      if(color == ""): break
+      rgbString += str(colorCompiler.rgbToHex(eval(color))) + "\n";
+      rrPalette[eval(color)] = i;
+      count -= 1; # count-- doesn't work for some reason???
+      with open("Hex Colors.txt", "w", encoding="UTF-8") as file:
+          file.write(rgbString);
+    except(IOError):
+          print("Error!! Please try restart the program and input the color values correctly, thank you.");
+          time.sleep(4);
+          exit();
 
+elif(option == 3):
+  rrPalette = {};
+  for i, ln in enumerate(open("Hex Colors.txt", "r", encoding="UTF-8").readlines()):
+    rrPalette[colorCompiler.hexToRGB(ln)] = SYMBOLS_LIST[i];
 # use default colors.
 else: pass;
 
@@ -431,8 +297,7 @@ importMethod = int(input("1. Variable Import\n2. List Create Import\n> "))
 # set list size.
 list_size = 50
 if(importMethod == 2): list_size = 64;
-# Load the image
-img = Image.open(IMAGE_PATH);
+  
 # Write the compiled strings to the text file
 write_strings_to_file(stringEncoder(list_size))
 
